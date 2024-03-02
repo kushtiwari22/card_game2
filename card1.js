59b1efb4ebee7fe591f1f196f45b3e272b3c3bfc
kushtiwari22 <152945457+kushtiwari22@users.noreply.github.com>
2024-03-03 02:28:06 +0530

    Update card1.js

--- card1.js
+++ card1.js
@@ -5,29 +5,32 @@ const team1_button = document.getElementById("pack3");
 const team2_button = document.getElementById("pack6");
 let choice1;
 let choice2;
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < 12; i++) {
     user1choice.push(document.getElementById(String(i)));
-    user2choice.push(document.getElementById(String(i + 3)));
+    user2choice.push(document.getElementById(String(i + 13)));
 }
 document.getElementById("score1").textContent=0;
 document.getElementById("score2").textContent=0;
 team1_button.addEventListener("click", function1);
 team2_button.addEventListener("click", function2);
-// let king1=document.getElementsByClassName("king1");
-// let king2=document.getElementsByClassName("king2");
+let h=0;
 function function1() {
-    choice1 = user1choice[Math.floor(Math.random() * 3)];
+    if(h==0){
+    choice1 = user1choice[Math.floor(Math.random() * 13)];
     choice1.style.marginTop = "30vh";
+    h=1;
+    }
 }
-
-// Function to handle team 2 choice
 function function2() {
-    choice2 = user2choice[Math.floor(Math.random() * 3)];
+    if(h==1){
+    choice2 = user2choice[Math.floor(Math.random() * 13)];
     choice2.style.marginTop = "30vh";
     score(choice1, choice2);
+    h=0;
+    }
 }
 function score(choice1, choice2) {
-  const mark1 = parseInt(choice1.id) + 3;
+  const mark1 = parseInt(choice1.id) + 13;
   const mark2 = parseInt(choice2.id);
 
   if (mark1 > mark2) {
